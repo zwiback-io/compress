f9835978c6f51c44a44ccaa09e11ad0f9adb3330
wadafacc <tf@aduna.ch>
2024-11-21 17:05:55 +0100

    hmmm

--- src/data/structs.rs
+++ src/data/structs.rs
@@ -7,3 +7,14 @@ pub struct Data {
   pub expiry: Option<i64>,
   pub slug: String
 }
+
+// Specify the fully-qualified path for the table_name attribute#[derive(Insertable)]
+#[diesel(table_name = schema::human)]
+pub struct NewHuman {
+    pub first_name: String,
+    pub last_name: String,
+    pub age: i32,
+    pub username: String,
+    pub email: String,
+    pub location: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,33 @@
 #[macro_use] extern crate rocket;
+extern crate diesel;
 
 use rocket::{http::Status, response::Redirect, serde::json::Json};
 
 use data::structs::*;
+use data::db::*;
 
 mod data;
 
 
 #[launch]
 fn rocket() -> _ {
-  rocket::build().mount("/", routes![new_shortened, get_link])
+  let mut connection = connect();
+  let new_human = NewHuman {
+      first_name: String::from("John"),
+      last_name: String::from("Doe"),
+      age: 30,
+      username: String::from("johndoe"),
+      email: String::from("john.doe@example.com"),
+      location: String::from("New York"),
+  };
+  diesel::insert_into(structs::::table).values(&new_human).execute(&mut connection).expect("Error saving new human");
+
+
+  rocket::build().mount("/", routes![new, get_link])
 }
 
 #[post("/new", data = "<data>")]
-fn new_shortened(data: Json<Data>) -> Status {
+fn new(data: Json<Data>) -> Status {
   Status::ImATeapot
 }
 
